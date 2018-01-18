ca0dec5ac7044f6faeb04ce120946b6832bffa83
cumminj1 <33314825+cumminj1@users.noreply.github.com>
2018-01-18 15:56:50 +0000

    Add files via upload
    The program now works with susceptibility and magnetisation, though susc tail is a little too big

--- lattice.py
+++ lattice.py
@@ -27,30 +27,34 @@ N= 	no of x elements in the array
 M=	no of y elements in the array
 B=	the magnetic field applied to the lattice
 T= 	the temperature applied to the lattice""" 
-N=100
-M=100
-steps=100*N*M
+N=60
+M=60
+steps=800*N*M
 sweeps= steps/(N*M)
 Kb=1
 T=1
-B=0
+B=1
 J=1
 
 	
 #empty list to store energy values		
 
 class ising_model_lattice:
-		
-#=======================================================================================
+#==============================================================================================		
+#==============================initialisation==============================================
+#==============================================================================================
+	
 	def __init__(self, N, M, B, T, Energy):                            #the init (says arguments etc)
 		self.N=N
 		self.M=M
 		self.B=B
 		self.T=T
-		self.Energy=Energy
+		
 
-#=======================================================================================	
-
+#==============================================================================================
+#===========================intial matrix maker=============================================	
+#==============================================================================================
+	
 	def vanilla_lattice_make(N,M):                             #fucntion which makes lattice
 		matrix=np.zeros((N,M))
 		for i in range(0,N):
@@ -59,8 +63,12 @@ class ising_model_lattice:
 		return matrix
 	
 	state_init=vanilla_lattice_make(N,M)
-#======================================================================================	 
-	"""def metro_alg(steps, state_init, T, B, J):
+
+#==============================================================================================
+#============================metropolois algorithm===============================================	
+#============================================================================================== 
+	
+	def metro_alg(steps, state_init, T, B, J):
 		for i in range(0,steps):
 			#choose the initial spin si
 			#note that need to go 1 higher with randint
@@ -103,33 +111,60 @@ class ising_model_lattice:
 			
 			#adding a progress monitor to the loop, with overwrite in terminal
 			print "Sweeps are " + str((100*float(i))/steps) + '% complete' + "\r" ,
-			i=+1"""
-#====================================================================================================		
-	def energy(steps, state_init, T, B, J):
-		#the energy of 1 flip is as above
-		for i in range (0,N):
-			for j in range (0,M):			
-			
-				totalE=[]
-
-				x_pos=i
-				y_pos=j
-				si=state_init[x_pos,y_pos]
-
-				#think about the effect of its nearest neigbours
-				#add the modulus division as periodic boundary cond.
-				nearest= (state_init[(x_pos+1)%N,y_pos]+ 
-					state_init[(x_pos-1)%N,y_pos]+
-					state_init[x_pos,(y_pos+1)%M]+
-					state_init[x_pos,(y_pos-1)%M])
-				solo_energy= si*B - J*si*nearest
-				totalE.append(solo_energy)
-		E=sum(totalE)/(N*M)
-		print E
-		print(totalE[0])
-	enertest=energy(steps, state_init, T, B, J)
-	print(enertest)
+			i=+1
+		return state_init
 	
+	#state_final_check=metro_alg(steps, state_init, T, B, J)
+	#plt.imshow(state_final_check)
+	#plt.show()
+
+#==============================================================================================	
+#==========================================magnetisation=========================================
+#==============================================================================================
+	#here define the magnetisation as the sum of all spins, normalised, absolute
+	def magnetisation(matrix):
+		mags= abs(np.sum(matrix)/(N*M))
+		return mags
+
+	#create a temperature array over which we check the magnetisation
+	temp_array=np.arange(0.1,4,0.25)	
+	
+	#create a for loop of temperature arrays so we can see how magnetisation 
+	#changes as a function of temperature
+	#for T in (temp_array):
+	#	state_final=metro_alg(steps, state_init, T, B, J)
+	#	mag_= magnetisation(state_final)
+	#	print("the normalised magnetisation is equal to " + str(mag_) + " for Temperature= " + str(T))
+	#	plt.plot(T, mag_, 'ro')
+	
+	#preparing the plot of magnetisation as a function of temperature
+	#plt.xlabel('Temperature')
+	#plt.ylabel("net magnetisation")
+	#plt.title('magnetisation as function of temperature for an '+ str(N) + ' x ' + str(M) + ' matrix ')
+	#plt.show()
+
+#==============================================================================================	
+#=================================magnetic succeptibility===================================	
+#==============================================================================================
+
+	#Mag supt is defined as X= [1/T][<M**2>-<M>**2]
+	def succeptibility(matrix, T):
+		mags= abs((np.sum(matrix*matrix))/(N*M))
+		mags2= abs(np.sum((matrix)/(N*M))**2)
+		suscept = (1/T)*(mags-mags2)
+		return suscept
+
+	for T in (temp_array):
+		state_final=metro_alg(steps, state_init, T, B, J)
+		succ= succeptibility(state_final, T)
+		print("the mag susceptibility is equal to " + str(succ) + " for Temperature= " + str(T))
+		plt.plot(T, succ, 'ro')
+	#preparing the plot of mag suscept. as a function of temperature
+	plt.xlabel('Temperature')
+	plt.ylabel("mag suscept.")
+	plt.title('mag suscept. as function of temperature for an '+ str(N) + ' x ' + str(M) + ' matrix ')
+	plt.show()
+
 """#=======================================================================================================
 		#printing the graph of the final state
 		print("Now Plotting The Final state...                             ")
@@ -157,12 +192,12 @@ class ising_model_lattice:
 
 	plt.show()
 
-	metrotest=metro_alg(steps, state_init, T, B, J)
+	metrotest=metro_alg(steps, state_init, T, B, J)	#taken by cutpaste from the end	
 	#plt.imshow(metrotest)
 	plt.show()
-#==========================================================================
-"""
-	
+#=========================================================================="""
+
+
 	
 
 
