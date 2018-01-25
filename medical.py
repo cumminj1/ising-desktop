7de2092a71dad9a9b3952e8c455fa3f0c2c41c4c
cumminj1 <33314825+cumminj1@users.noreply.github.com>
2018-01-25 20:25:45 +0000

    virility and immunity workiing. need seed infection
    virility function needs seed. the random seed is generating all

--- medical.py
+++ medical.py
@@ -31,76 +31,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 from numpy import random
 
-N=20
-M=20
+N=200
+M=200
 steps=1000
-B=50		#herd immunity
-Vir=1		#disease virility
+immunity_rate=100	#herd immunity
+Virility=60.		#disease virility
 T=50
 J=1
 Kb=1
-mort_r=9999.0	#mortality rate in percent
-
+mort_r=99.0	#mortality rate in percent
+surv_r=50.       #the survivabiliity rate
 def healthy_population(N,M):
 	matrix=np.ones((N,M))
 	return matrix
 
 health=healthy_population(N,M)
-plt.imshow(health)
+"""plt.imshow(health)
 plt.colorbar()
+plt.ion()
 plt.show()
-
 """
-def infection(N, M,steps, state_init, B, Vir, T):
-	for i in range(0,steps):
-		#choose the initial spin si
-		#note that need to go 1 higher with randint
-		
-		#x_pos=np.random.randint(self.N+1)
-		#y_pos=np.random.randint(self.M+1)
-		x_pos=np.random.choice(N)
-		y_pos=np.random.choice(M)
-		si=state_init[x_pos,y_pos]
 
-		#think about the effect of its nearest neigbours
-		#add the modulus division as periodic boundary cond.
-		contact= (state_init[(x_pos+1)%N,y_pos]+ 
-			state_init[(x_pos-1)%N,y_pos]+
-			state_init[x_pos,(y_pos+1)%M]+
-			state_init[x_pos,(y_pos-1)%M])
-			#state_init[(x_pos+1)%N,(y_pos+1)%M]+
-			#state_init[(x_pos-1)%N,(y_pos-1)%M]+
-			#state_init[(x_pos+1)%N,(y_pos-1)%M]+
-			#state_init[(x_pos-1)%N,(y_pos+1)%M])
-		#print(contact)
-		#now need to try to flip the spin and
-		#see how the energy changes
-		#transmission= 1-(1/Vir)+ B
-		#transmission= -B + 1*si*(contact/8)*Vir
-		#transmission= +2*contact*B - 2*si*(contact/2)*Vir
-
-
-		dE= +si*B - si*contact*Vir
-		print(dE)
-		#set the test conditions
-	 	#print random.random()
-		# if energy is less than 0
-		#accetp the flip
-		if dE <= 0:
-			si *= -1
-		#if energy is greater than 0
-		#only accept the flip with a certain probability:
-		elif random.random() < np.exp((1*dE)/(1*(T))):
-			si *= -1
-		else: 
-			si*=1
-		state_init[x_pos,y_pos]= si	
-		#adding a progress monitor to the loop, with overwrite in terminal
-		#print "Sweeps are " + str((100*float(i))/steps) + '% complete' + "\r" ,
-		i=+1
-	return state_init
-"""
-def metro_alg(N, M,steps, state_init, T, B, J, Kb):
+def metro_alg(N, M,steps, state_init, immunity_rate, Virility):
 	for i in range(0,steps):
 		#choose the initial spin si
 		#note that need to go 1 higher with randint
@@ -120,19 +72,20 @@ def metro_alg(N, M,steps, state_init, T, B, J, Kb):
 
 		#now need to try to flip the spin and
 		#see how the energy changes
-		dE= -2*si*B - 2*J*si*nearest
+		inf= immunity_rate -Virility
+		#inf=(immunity_rate-Virility)*nearest
 
 		#set the test conditions
 	 	#print random.random()
 		# if energy is less than 0
 		#accetp the flip
-		if dE <= 0:
+		if inf <= 0 and nearest < 4:
 			si = -1
 		#if energy is greater than 0
 		#only accept the flip with a certain probability:
 		
 		#the probability of being an independednt contractor of the disease				
-		elif random.random() < 1/(N*M):
+		elif random.random() < float(Virility/100.):
 			si *= -1
 
 		else: 
@@ -149,10 +102,12 @@ def metro_alg(N, M,steps, state_init, T, B, J, Kb):
 	
 
 
-check= metro_alg(N,M, steps, health, T, B, J, Kb)
-plt.imshow(check, interpolation='none')
-plt.colorbar()
-plt.show()
+check= metro_alg(N,M, steps, health, immunity_rate, Virility)
+"""plt.imshow(check, interpolation='none',cmap=plt.cm.get_cmap('RdYlGn', 2))
+plt.colorbar(ticks=range(-1,2), label= 'Healthy and Infected')
+
+plt.ion()
+plt.show()"""
 
 
 #death is a function that affects the infected population 
@@ -176,14 +131,34 @@ def death(N,M,steps,matrix, mortality_rate):
 		i+=1
 	return matrix
 death_check= death(N,M,steps, check, mort_r)
-plt.imshow(death_check, interpolation='none')
-plt.colorbar()
+plt.imshow(death_check, interpolation='none', cmap=plt.get_cmap('RdYlGn',3))
+plt.colorbar(ticks=range(-2,2), label= 'Healthy and Infected')
+plt.ion()
 plt.show()
 
+def recovery(N,M,steps,matrix, survival_rate):
+	for i in range (steps):	
+		x_pos=np.random.choice(N)
+		y_pos=np.random.choice(M)
+		si=matrix[x_pos,y_pos]
+		rand=random.random()
+  
+  
+		if si==(-1) and (rand< float(survival_rate/100)):
+				si*=-1
+              
+		else:
+				si*=1
+		matrix[x_pos,y_pos]=si
+		i+=1
+	return matrix
+ 
 
-
-
-
+"""recovtest= recovery(N,M, steps, check, surv_r)
+plt.imshow(recovtest, interpolation='none')
+plt.colorbar()
+plt.show()
+"""
 
 
 
